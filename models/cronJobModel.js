f67a65fe0ef997667a8e6e49e24d521ea2630bce
Abhinav Sharma <abhinav291sharma@gmail.com>
2024-08-02 16:22:56 +0530

    Fixed: Schema Init Issues

--- models/cronJobModel.js
+++ models/cronJobModel.js
@@ -1,32 +1,17 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const cronJobSchema = new mongoose.Schema({
-    mailModelDetails: {
-        //to mention the date and time on which mail has to be sent
-        dateTime: {
-            type: Date,
-            required: true
-        },
-        //to keep track of how frequently the mail has to be sent
-        freqType: {
-            type: String,
-            required: true
-        },
-        //to keep day on which mail is to be sent when it comes to monthly, weekly or yearly schedules
-        freqDay: {
-            type: Number,
-        },
-        subject: {
-            type: String,
-            required: true
-        },
-        body: {
-            type: String,
-            required: true
-        },
-        attachments: [String]
-    }
+const cronJobSchema = new Schema({
+    //to mention the date and time on which mail has to be sent
+    dateTime: { type: Date, required: true },
+    //to keep track of how frequently the mail has to be sent
+    freqType: { type: String, required: true },
+    //to keep day on which mail is to be sent when it comes to monthly, weekly or yearly schedules
+    freqDay: { type: Number},
+    subject: { type: String, required: true },
+    body: { type: String, required: true },
+    attachments: [String]
 });
 
-const cronJob = new mongoose.Schema('cronJobModel', cronJobSchema);
+const cronJob = new mongoose.model('Cron', cronJobSchema);
 module.exports = cronJob;
--- models/mailModel.js
+++ models/mailModel.js
@@ -1,31 +1,15 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const mailSchema = new mongoose.Schema({
-    dateTime: {
-        type: Date,
-        required: true
-    },
-    freqType: {
-        type: String,
-        required: true
-    },
-    freqDay: {
-        type: Number,
-    },
-    subject: {
-        type: String,
-        required: true
-    },
-    body: {
-        type: String,
-        required: true
-    },
+const mailSchema = new Schema({
+    dateTime: { type: Date, required: true },
+    freqType: { type: String, required: true },
+    freqDay: { type: Number },
+    subject: { type: String, required: true },
+    body: { type: String, required: true },
     attachments: [String],
-    cronJobId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'cronJob',
-    }
+    cronJobId: { type: Schema.Types.ObjectId, ref: "cronJob" }
 });
 
-const mailModel = new mongoose.Schema("mailModel", mailSchema);
+const mailModel = new mongoose.model("Mail", mailSchema);
 module.exports = mailModel;
