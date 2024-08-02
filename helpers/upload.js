e31ff32da245b1dde5cb85ead95581d6966498e5
Abhinav Sharma <abhinav291sharma@gmail.com>
2024-08-02 16:10:45 +0530

    Added: Helper function and firebase initialization files

---
+++ helpers/upload.js
@@ -0,0 +1,12 @@
+
+
+//to upload files to firestore
+const uploadFiles = (attachments) => {
+    try {
+        
+    } catch (error) {
+        console.log("Error uploading files: ", error);
+    }
+}
+
+module.exports = uploadFiles;
