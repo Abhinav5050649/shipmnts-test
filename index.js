4bfb2e1342cbc417ab84e5e5964b03bd27e76930
Abhinav Sharma <abhinav291sharma@gmail.com>
2024-08-02 14:58:34 +0530

    Updated: index.js to include db connection initialization file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+import connect from './dbConfig/dbConfig';
+
+connect();
 
 app.use(express.json());
 app.use(cors());
