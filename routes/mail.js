c1e73875a179b9d637cbe5fb699306d77d58b00d
Abhinav Sharma <abhinav291sharma@gmail.com>
2024-08-02 16:11:19 +0530

    Modified: post route

--- routes/mail.js
+++ routes/mail.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const mailModel = require("../models/mailModel");
+const cronJob = require("../models/cronJobModel");
+const uploadFiles = require("../helpers/upload");
+
 require('dotenv').config();
 
 router.get(`/scheduled-emails/:id`, async(req, res) => {
     try {
+        const mailData = await mailModel.findById(req.params.id);
         
+        return (mailData)   ?   res.status(200).send(mailData).json({message: "Scheduled Mail Found!"}) : res.status(404).json({message: "Scheduled Mail NOT Found!"});
+
     } catch (error) {
         return res.status(500).json({message: "Internal Server Error!"});
     }
@@ -12,7 +19,10 @@ router.get(`/scheduled-emails/:id`, async(req, res) => {
 
 router.get(`/scheduled-emails`, async(req, res) => {
     try {
-        
+        const mailsData = await mailModel.find();
+
+        return res.status(200).send(mailsData).json({message: "All mails!"});
+
     } catch (error) {
         return res.status(500).json({message: "Internal Server Error!"});
     }
@@ -20,7 +30,23 @@ router.get(`/scheduled-emails`, async(req, res) => {
 
 router.post(`/schedule-email`, async(req, res) => {
     try {
-        
+        const {receipient, dateTime, freqType, freqDay, subject, body} = req.body;
+        const attachments = req.files;
+
+        let links = [];
+        if (attachments.length != 0)    links = uploadFiles(attachments);
+
+
+        const newMail = await mailModel.create({
+            receipient,
+            dateTime,
+            freqType,
+            freqDay,
+            subject,
+            body    
+        });
+
+
     } catch (error) {
         return res.status(500).json({message: "Internal Server Error!"});
     }
